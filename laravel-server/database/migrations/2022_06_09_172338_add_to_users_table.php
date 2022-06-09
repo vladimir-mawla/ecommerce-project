744858b45ea656c72171cbde163f52323acb2fdc
vladimir <vladimirmawla2@gmail.com>
2022-06-09 20:25:35 +0300

    Adding user type column to users table

--- laravel-server/database/migrations/2022_06_09_172338_add_to_users_table.php
+++ laravel-server/database/migrations/2022_06_09_172338_add_to_users_table.php
@@ -14,7 +14,7 @@ class AddToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->integer('user_type')->after('password');
         });
     }
 
