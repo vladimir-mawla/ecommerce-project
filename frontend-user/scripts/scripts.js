25ff9a526da60296e4f513882d15e03da4e92003
vladimir <vladimirmawla2@gmail.com>
2022-06-10 20:27:51 +0300

    Adding a submit function and adding empty data alert to it

--- frontend-user/scripts/scripts.js
+++ frontend-user/scripts/scripts.js
@@ -1,3 +1,9 @@
 var login_username = document.getElementById("login_email")
 var login_password = document.getElementById("login_password")
 document.getElementById("login_button").addEventListener("click", submit)
+function submit() {
+    if (login_username.value == "" || login_password.value == "") {
+      
+        alert("empty data")
+    }
+  }
