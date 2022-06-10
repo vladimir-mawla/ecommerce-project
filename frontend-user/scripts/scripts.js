118984447bc1a770b0eac557a51d0fe32c917753
vladimir <vladimirmawla2@gmail.com>
2022-06-10 20:06:36 +0300

    Assigning variables for login elements

---
+++ frontend-user/scripts/scripts.js
@@ -0,0 +1,3 @@
+var login_username = document.getElementById("login_email")
+var login_password = document.getElementById("login_password")
+document.getElementById("login_button").addEventListener("click", submit)
